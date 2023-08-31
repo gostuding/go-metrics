74ac2fde4f0fa7529585fd7e87db36607eb5a900
gostuding <mag-nat1@rambler.ru>
2023-08-31 13:45:52 +0300

    multichecks add

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+
+	"mycheck/errcheckanalyzer"
+
+	"honnef.co/go/tools/staticcheck"
+)
+
+// Config — имя файла конфигурации.
+const Config = `config.json`
+
+// ConfigData описывает структуру файла конфигурации.
+type ConfigData struct {
+	Staticcheck []string
+}
+
+func main() {
+	mychecksStaticChecks, err := readConfigFile()
+	if err != nil {
+		log.Printf("file 'config.json' read error: %v\n", err)
+	}
+	// определяем map подключаемых правил
+	checks := map[string]bool{
+		"SA5000": true,
+		"SA6000": true,
+		"SA9004": true,
+	}
+	for _, v := range staticcheck.Analyzers {
+		if checks[v.Analyzer.Name] {
+			mychecksStaticChecks = append(mychecksStaticChecks, v.Analyzer)
+		}
+	}
+	mychecksStaticChecks = append(mychecksStaticChecks, errcheckanalyzer.ErrCheckAnalyzer)
+	mychecksStaticChecks = append(mychecksStaticChecks, printf.Analyzer)
+	mychecksStaticChecks = append(mychecksStaticChecks, shadow.Analyzer)
+	mychecksStaticChecks = append(mychecksStaticChecks, structtag.Analyzer)
+	mychecksStaticChecks = append(mychecksStaticChecks, shift.Analyzer)
+
+	multichecker.Main(
+		mychecksStaticChecks...,
+	)
+}
+
+func readConfigFile() ([]*analysis.Analyzer, error) {
+	configChecks := make([]*analysis.Analyzer, 0)
+
+	appfile, err := os.Executable()
+	if err != nil {
+		return configChecks, fmt.Errorf("get file name error: %w", err)
+	}
+	data, err := os.ReadFile(filepath.Join(filepath.Dir(appfile), Config))
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("config.json is not exist")
+			return configChecks, nil
+		}
+		return configChecks, fmt.Errorf("read config file error: %w", err)
+	}
+	var cfg ConfigData
+	if err = json.Unmarshal(data, &cfg); err != nil {
+		return configChecks, fmt.Errorf("config file json incorrect: %w", err)
+	}
+
+	checks := make(map[string]bool)
+	for _, v := range cfg.Staticcheck {
+		checks[v] = true
+	}
+	// добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
+	for _, v := range staticcheck.Analyzers {
+		if checks[v.Analyzer.Name] {
+			configChecks = append(configChecks, v.Analyzer)
+		}
+	}
+	return configChecks, nil
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/stretchr/testify v1.8.3
 	go.uber.org/zap v1.24.0
+	golang.org/x/tools v0.9.4-0.20230601214343-86c93e8732cc
+	honnef.co/go/tools v0.4.5
 )
 
 require (
@@ -24,6 +26,8 @@ require (
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
